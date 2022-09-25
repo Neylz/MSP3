8f5e8a5643edce27810d420f4b5b4331c527f1a5
Neylz <neylzvii@gmail.com>
2022-09-25 18:49:05 +0200

    init

---
+++ src/main/java/me/neylz/msp3/Msp3.java
@@ -0,0 +1,18 @@
+package me.neylz.msp3;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Msp3 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Salut Nassim");
+        getLogger().warning("fdp");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
