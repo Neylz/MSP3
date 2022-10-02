c1bf6cfe80be38793c436c199d43a69f3aa73be2
Neylz <neylzvii@gmail.com>
2022-10-02 17:09:41 +0200

    Implement teams with the vanilla system

--- src/main/java/me/neylz/msp3/Msp3.java
+++ src/main/java/me/neylz/msp3/Msp3.java
@@ -6,19 +6,44 @@ import me.neylz.msp3.commands.AdminCamCommand;
 import me.neylz.msp3.commands.FactionCommand;
 import me.neylz.msp3.commands.tabcompletion.AdminCamCompletion;
 import me.neylz.msp3.commands.tabcompletion.FactionTabCompletion;
+import me.neylz.msp3.events.InventoryEvents;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.KeybindComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+import org.bukkit.scoreboard.Team;
 
+import java.awt.*;
 import java.util.Objects;
+
+import static org.bukkit.scoreboard.Team.Option.*;
 
 
 public final class Msp3 extends JavaPlugin {
 
     public static Msp3 instance;
-
     public static Msp3 getInstance() {
         return instance;
     }
+
+
+    //factions
+    public Team terreFaction;
+    public Team eauFaction;
+    public Team planteFaction;
+    public Team feuFaction;
+    public Team lumiereFaction;
+
+    public ScoreboardManager scoreboardManager;
+    public Scoreboard scoreboard;
+
+
 
     @Override
     public void onEnable() {
@@ -31,6 +56,33 @@ public final class Msp3 extends JavaPlugin {
         Objects.requireNonNull(getCommand("faction")).setExecutor(new FactionCommand());
         Objects.requireNonNull(this.getCommand("faction")).setTabCompleter(new FactionTabCompletion());
 
+        //events
+        getServer().getPluginManager().registerEvents(new InventoryEvents(), this);
+
+
+        //teams
+        scoreboardManager = Bukkit.getScoreboardManager();
+        scoreboard = scoreboardManager.getMainScoreboard();
+
+        //detect if the team exists-> create it or get it
+        if (scoreboard.getTeam("Terre") == null) terreFaction = scoreboard.registerNewTeam("Terre"); else terreFaction = scoreboard.getTeam("Terre");
+        if (scoreboard.getTeam("Feu") == null) feuFaction = scoreboard.registerNewTeam("Feu"); else feuFaction = scoreboard.getTeam("Feu");
+        if (scoreboard.getTeam("Eau") == null) eauFaction = scoreboard.registerNewTeam("Eau"); else eauFaction = scoreboard.getTeam("Eau");
+        if (scoreboard.getTeam("Plante") == null) planteFaction = scoreboard.registerNewTeam("Plante"); else planteFaction = scoreboard.getTeam("Plante");
+        if (scoreboard.getTeam("Lumiere") == null) lumiereFaction = scoreboard.registerNewTeam("Lumiere"); else lumiereFaction = scoreboard.getTeam("Lumiere");
+
+        //apply parameters/update them
+        applyFactionParameters(terreFaction, NamedTextColor.DARK_RED, "\uE022");
+        applyFactionParameters(feuFaction, NamedTextColor.RED, "\uE025");
+        applyFactionParameters(eauFaction, NamedTextColor.DARK_AQUA, "\uE023");
+        applyFactionParameters(planteFaction, NamedTextColor.DARK_GREEN, "\uE024");
+        applyFactionParameters(lumiereFaction, NamedTextColor.GOLD, "\uE021");
+
+
+
+
+
+        //log
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Mazened Survival Plugin 3 Enabled");
     }
 
@@ -38,6 +90,23 @@ public final class Msp3 extends JavaPlugin {
     public void onDisable() {
         getServer().getConsoleSender().sendMessage(ChatColor.RED + "Mazened Survival Plugin 3 Disabled");
 
+
+    }
+
+
+    private void applyFactionParameters (Team faction, NamedTextColor color, String prefix) {
+        faction.color(color);
+
+        Component newPrefix = Component.text(prefix, TextColor.color(255,255,255)).font(Key.key("msmp", "custom"));
+        newPrefix = newPrefix.append(Component.text(" ").font(Key.key(Key.MINECRAFT_NAMESPACE, "default")));
+        faction.prefix(newPrefix);
+
+        faction.setOption(NAME_TAG_VISIBILITY, Team.OptionStatus.ALWAYS);
+        faction.setOption(COLLISION_RULE, Team.OptionStatus.ALWAYS);
+        faction.setOption(DEATH_MESSAGE_VISIBILITY, Team.OptionStatus.ALWAYS);
+
+        faction.setAllowFriendlyFire(true);
+        faction.setCanSeeFriendlyInvisibles(false);
 
     }
 }
--- src/main/java/me/neylz/msp3/events/InventoryEvents.java
+++ src/main/java/me/neylz/msp3/events/InventoryEvents.java
@@ -1,13 +1,44 @@
 package me.neylz.msp3.events;
 
+import me.neylz.msp3.inventories.FactionSelection;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.scoreboard.Team;
 
 public class InventoryEvents implements Listener {
 
     @EventHandler
     public void onClick(InventoryClickEvent e) {
+        if (e.getClickedInventory() == null) { return; }
+
+        if (e.getClickedInventory().getHolder() instanceof FactionSelection) {      // faction selection screen
+            e.setCancelled(true);
+            Player player = (Player) e.getWhoClicked();
+
+            if (e.getCurrentItem() == null) { return; }
+
+            if (e.getCurrentItem().getType() == Material.KNOWLEDGE_BOOK) {
+                switch (e.getCurrentItem().getItemMeta().getCustomModelData()) {
+                    default :
+                        player.sendMessage(ChatColor.RED + "error : CustomModelData not defined");
+                        break;
+                    case 1: //terre
+                        if (player.getScoreboard().getTeams() == null) {
+
+                        }
+                        break;
+                    case 2: //eau
+                    case 3: //plante
+                    case 4: //feu
+                }
+            }
+        }
 
     }
 }
